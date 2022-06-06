96b3babfb2578cd311416f69d725293f65b70a7a
Julianno <juliannojungle@gmail.com>
2022-06-06 13:21:07 -0300

    :construction: Partial

--- src/opencc.cpp
+++ src/opencc.cpp
@@ -1,23 +1,23 @@
 #include "opencc.h"
 
 int main(void) {
-  int screenWidth = 800;
-  int screenHeight = 450;
-  Color textColor = LIGHTGRAY;
+    int screenWidth = 800;
+    int screenHeight = 450;
+    Color textColor = LIGHTGRAY;
 
-  InitWindow(screenWidth, screenHeight, string("raylib [core] example - basic window").c_str());
+    InitWindow(screenWidth, screenHeight, string("raylib [core] example - basic window").c_str());
 
-  SetTargetFPS(60);
+    SetTargetFPS(60);
 
-  while (WindowShouldClose())
-  {
-    BeginDrawing();
+    while (WindowShouldClose())
     {
-        ClearBackground(RAYWHITE);
-        DrawText(string("Congrats! You created your first window!").c_str(), 190, 200, 20, textColor);
+        BeginDrawing();
+        {
+            ClearBackground(RAYWHITE);
+            DrawText(string("Congrats! You created your first window!").c_str(), 190, 200, 20, textColor);
+        }
+        EndDrawing();
     }
-    EndDrawing();
-  }
 
-  return 0;
+    return 0;
 }
--- src/opencc.h
+++ src/opencc.h
@@ -2,5 +2,6 @@
 using namespace std;
 
 extern "C" {
-  #include "./dependencies/raylib/src/raylib.h"
+    #include "./dependencies/raylib/src/raylib.h"
+    #include "./dependencies/pigpio/pigpio.h"
 }
